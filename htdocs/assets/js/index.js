6477d84dc32e380f358e70abdc4f3175e779b153
kienht@runsystem.net <Kien@2505>
2025-06-05 16:23:25 +0700

    update

--- htdocs/assets/js/index.js
+++ htdocs/assets/js/index.js
@@ -1,6 +1,71 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded');
-  
+    const swipArrivals = tns({
+      container: '#products-arrivals',
+      direction: 'horizontal',
+      slideBy: 'page',
+      mouseDrag: true,
+      controls: false,
+      prevButton: false,
+      nextButton: false,
+      nav: false,
+      loop: true,
+      arrowKeys:false,
+      fixedWidth: 295,
+      gutter: 20,
+      center: true,
+      responsive: {
+        320: {
+          edgePadding: 0,
+          fixedWidth: 198,
+          gutter: 16,
+          center: true,
+        },
+        480: {
+          fixedWidth: 250,
+          gutter: 16,
+          center: true,
+          edgePadding: 0,
+        },
+        750: {
+          edgePadding: 0,
+          fixedWidth: 295,
+          gutter: 20,
+        },
+      },
+    });
+    const swipSelling = tns({
+      container: '#products-selling',
+      direction: 'horizontal',
+      slideBy: 'page',
+      mouseDrag: true,
+      controls: false,
+      prevButton: false,
+      nextButton: false,
+      nav: false,
+      loop: true,
+      arrowKeys:false,
+      fixedWidth: 295,
+      gutter: 20,
+      center: true,
+        320: {
+          edgePadding: 0,
+          fixedWidth: 198,
+          gutter: 16,
+          center: true,
+        },
+        480: {
+          fixedWidth: 250,
+          gutter: 16,
+          center: true,
+          edgePadding: 0,
+        },
+        750: {
+          edgePadding: 0,
+          fixedWidth: 295,
+          gutter: 20,
+        },
+    });
     const swiper = tns({
       container: '#promotion-slider',
       direction: 'horizontal',
@@ -12,28 +77,29 @@ document.addEventListener('DOMContentLoaded', () => {
       nav: false,
       loop: true,
       arrowKeys:false,
-      fixedWidth: 400,
+      fixedWidth: 358,
       gutter: 20,
-      center: true,
-      // responsive: {
-      //   320: {
-      //     edgePadding: 0,
-      //     fixedWidth: 300,
-      //     gutter: 50,
-      //     center: true,
-      //   },
-      //   750: {
-      //     fixedWidth: 380,
-      //     gutter: 78,
-      //     center: true,
-      //     edgePadding: 0,
-      //   },
-      //   1024: {
-      //     edgePadding: 0,
-      //     fixedWidth: 240,
-      //     gutter: 74,
-      //   },
-      // },
+      responsive: {
+        320: {
+          items: 1,
+          edgePadding: 0,
+          fixedWidth: 250,
+          gutter: 15,
+          center: true,
+        },
+        390: {
+          items: 1,
+          fixedWidth: 358,
+          gutter: 0,
+          edgePadding: 0,
+        },
+        1024: {
+          edgePadding: 0,
+          fixedWidth: 400,
+          gutter: 20,
+          center: true,
+        },
+      },
     });
     document.getElementById('prevBtn').addEventListener('click', () => {
       swiper.goTo('prev');
